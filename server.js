bc1c51739bb90fac094e6452074957de099192bf
Bismarck Berrios <bisberrios@gmail.com>
2020-12-02 21:17:03 -0600

    Primer commit!

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    aws = require('aws-sdk'),
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
+    multerS3 = require('multer-s3');
+
+aws.config.update({
+    secretAccessKey: 'CSjTaQjuW9guZCarztQG7boa22PWZiaS1KiaQXjX',
+    accessKeyId: 'AKIA37SVVXBHQQGI3OSN',
+    region: 'us-east-1'
+});
+
+var app = express(),
+    s3 = new aws.S3();
+
+app.use(bodyParser.json());
+
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'cloud-cube',
+        key: function (req, file, cb) {
+            console.log(file.originalname);
+            cb(null, 'vystoq0cy0tf/public/testtt.pdf'); //use Date.now() for unique file keys
+        }
+    })
+});
+
+//open in browser to see upload form
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');//index.html is inside node-cheat
+});
+
+//use by upload form
+app.post('/upload', upload.array('myFile',1), function (req, res, next) {
+    res.send("Uploaded!");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
