9242c13329f09672c4ac6fdfd4bf2d1253dbbca8
Bismarck <45794569+bisberrios@users.noreply.github.com>
2020-12-11 07:23:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var express = require('express'),
     multerS3 = require('multer-s3');
 
 aws.config.update({
-    secretAccessKey: secretAccessKey,
-    accessKeyId: accessKeyId,
+    secretAccessKey: 'CSjTaQjuW9guZCarztQG7boa22PWZiaS1KiaQXjX',
+    accessKeyId: 'AKIA37SVVXBHQQGI3OSN',
     region: 'us-east-1'
 });
 
@@ -21,7 +21,7 @@ var upload = multer({
         bucket: 'cloud-cube',
         key: function (req, file, cb) {
             console.log(file.originalname);
-            cb(null, 'path' + 'testtt.pdf'); //use Date.now() for unique file keys
+            cb(null, 'vystoq0cy0tf/public/testtt.pdf'); //use Date.now() for unique file keys
         }
     })
 });
@@ -39,3 +39,52 @@ app.post('/upload', upload.array('myFile',1), function (req, res, next) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+
+const fs = require('fs');
+const path = require("path");
+
+/*const s3 = new AWS.S3({
+    accessKeyId: 'AKIA37SVVXBHQQGI3OSN',
+    secretAccessKey: 'CSjTaQjuW9guZCarztQG7boa22PWZiaS1KiaQXjX'
+});*/
+
+
+
+const absoluteFilePath = "/home/bismarck/Escritorio/test/pdfeditable.pdf";
+const uploadFile = (file) => {
+    const filePath = path.normalize(path.join(__dirname, file));
+    fs.readFile(filePath, (err, data) => {
+     if (err) throw err;
+     const params = {
+         Bucket: 'cloud-cube', // pass your bucket name
+         Key: `vystoq0cy0tf/public/${file}_${new Date().getMilliseconds()}.pdf`, // file will be saved in <folderName> folder
+         Body: data
+     };
+      s3.upload(params, function (s3Err, data) {
+        if (s3Err) throw s3Err
+        console.log(`File uploaded successfully at ${data.Location}`);
+        return data.Location;
+    });
+  });
+};
+
+
+
+app.post("/aws", (req, res) => {
+    try {
+        const file = req.body.file;
+        const urlPDF = uploadFile(file);
+        if(urlPDF) {console.log(urlPDF)
+            res.statusCode = 200;
+            res.send(urlPDF);}	
+    } catch (error) {
+      res.contentType("json");
+      res.json(error.message);
+    }
+  });
+
+
+  //const file = "pdfeditable.pdf";
+  //const filePath = path.normalize(path.join(__dirname, file
+  //const file = req.body.file
