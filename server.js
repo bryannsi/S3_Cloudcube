99bf51e79612171bad87dcde81e3623b600bbec5
bryannsi <bryannsi@outlook.com>
2020-12-14 21:24:13 -0600

    Guardar archivo en la nube async

--- server.js
+++ server.js
@@ -1,90 +1,86 @@
-var express = require('express'),
-    aws = require('aws-sdk'),
-    bodyParser = require('body-parser'),
-    multer = require('multer'),
-    multerS3 = require('multer-s3');
+const express = require("express");
+const aws = require("aws-sdk");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const fs = require("fs");
+const path = require("path");
 
 aws.config.update({
-    secretAccessKey: 'CSjTaQjuW9guZCarztQG7boa22PWZiaS1KiaQXjX',
-    accessKeyId: 'AKIA37SVVXBHQQGI3OSN',
-    region: 'us-east-1'
+  secretAccessKey: "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",
+  accessKeyId: "AKIA37SVVXBHQQGI3OSN",
+  region: "us-east-1",
 });
 
-var app = express(),
-    s3 = new aws.S3();
+const app = express(),
+  s3 = new aws.S3();
 
 app.use(bodyParser.json());
 
-var upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'cloud-cube',
-        key: function (req, file, cb) {
-            console.log(file.originalname);
-            cb(null, 'vystoq0cy0tf/public/testtt.pdf'); //use Date.now() for unique file keys
-        }
-    })
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "cloud-cube",
+    key: function (req, file, cb) {
+      console.log(file.originalname);
+      cb(null, "vystoq0cy0tf/public/testtt.pdf"); //use Date.now() for unique file keys
+    },
+  }),
 });
 
 //open in browser to see upload form
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');//index.html is inside node-cheat
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html"); //index.html is inside node-cheat
 });
 
 //use by upload form
-app.post('/upload', upload.array('myFile',1), function (req, res, next) {
-    res.send("Uploaded!");
+app.post("/upload", upload.array("myFile", 1), function (req, res, next) {
+  res.send("Uploaded!");
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log(`Example app listening at http://localhost:${3000}`);
 });
-
-
-const fs = require('fs');
-const path = require("path");
 
 /*const s3 = new AWS.S3({
     accessKeyId: 'AKIA37SVVXBHQQGI3OSN',
     secretAccessKey: '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'
 });*/
 
-
-
-const absoluteFilePath = "/home/bismarck/Escritorio/test/pdfeditable.pdf";
-const uploadFile = (file) => {
+const uploadFile = async (file) => {
+  try {
     const filePath = path.normalize(path.join(__dirname, file));
-    fs.readFile(filePath, (err, data) => {
-     if (err) throw err;
-     const params = {
-         Bucket: 'cloud-cube', // pass your bucket name
-         Key: `vystoq0cy0tf/public/${file}_${new Date().getMilliseconds()}.pdf`, // file will be saved in <folderName> folder
-         Body: data
-     };
-      s3.upload(params, function (s3Err, data) {
-        if (s3Err) throw s3Err
-        console.log(`File uploaded successfully at ${data.Location}`);
-        return data.Location;
-    });
-  });
+    const data = fs.readFileSync(filePath);
+
+    const params = {
+      Bucket: "cloud-cube", // pass your bucket name
+      Key: `vystoq0cy0tf/public/${file}_${new Date().getMilliseconds()}.pdf`, // file will be saved in <folderName> folder
+      Body: data,
+    };
+
+    const stored = await s3.upload(params).promise();
+    console.log(`File uploaded successfully at ${data.Location}`);
+    return stored.Location;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
-
-
-app.post("/aws", (req, res) => {
-    try {
-        const file = req.body.file;
-        const urlPDF = uploadFile(file);
-        if(urlPDF) {console.log(urlPDF)
-            res.statusCode = 200;
-            res.send(urlPDF);}	
-    } catch (error) {
-      res.contentType("json");
-      res.json(error.message);
+app.post("/aws", async (req, res) => {
+  try {
+    const file = req.body.file;
+    const urlPDF = await uploadFile(file);
+    if (urlPDF) {
+      console.log(urlPDF);
+      res.statusCode = 200;
+      res.send(urlPDF);
+    } else {
+      res.statusCode = 500;
+      res.json({ message: "Ha ocurrido un error al guardar el archivo en la nube." });
     }
-  });
-
-
-  //const file = "pdfeditable.pdf";
-  //const filePath = path.normalize(path.join(__dirname, file
-  //const file = req.body.file
+  } catch (error) {
+    res.contentType("json");
+    res.json(error.message);
+  }
+});
